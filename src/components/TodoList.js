00dd1ffaaa0d209d420c9692a9b93a171562cb95
Truong <tranquoctruong812@gmail.com>
2023-08-24 22:19:06 +0700

    Map_Onclick

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,62 @@
 import React, { useState } from "react";
+import _ from "lodash";
 
 const TodoList = () => {
   //khai bao state
-  const [name, setName] = useState("");
+  const [todo, setTodo] = useState("");
+  const [listTodo, setListTodo] = useState([
+    { id: "todo1", name: "watching youtube" },
+    { id: "todo2", name: "Using youtube" },
+    { id: "todo3", name: "Reading book" },
+  ]);
 
-  //khởi tạo click
-  const handleClickBtn = (event, msg) => {
-    console.log(">>>run :", msg); //in ra run: button click
+  //Hàm tạo ra ID random
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+  //hàm Click submit: ấn vào là thêm giá trị mới nhập
+  const handleClickBtn = () => {
+    let todoId = randomIntFromInterval(4, 99999999999999); //gắn todoId=ID random ra ở hàm trên
+    let todoItem = {
+      id: `todo${todoId}`, //nối chuỗi Id cho giá trị mới nhập
+      name: todo,
+    };
+    setListTodo([...listTodo, todoItem]); //...listTodo: copy lại 3 ptu cũ , todoItem cập nhật lại giá trị mới: !spread operator
+  };
+
+  //hàm khi click vào giá trị là nó sẽ xóa
+  const handleDeletedTodo = (id) => {
+    let currentTodoList = _.clone(listTodo); //coppy lại ptu cũ ở trên
+    currentTodoList = currentTodoList.filter((item) => item.id !== id);
+    setListTodo(currentTodoList);
+  };
+
   return (
     <div>
-      <label>Name</label>
+      <label>Todo's Name</label>
       <input
-        value={name}
+        value={todo}
         type="text"
-        onChange={(event) => setName(event.target.value)}
+        onChange={(event) => setTodo(event.target.value)}
       />
-      <button
-        type="button"
-        onClick={(event) => {
-          handleClickBtn(event, "button click ");
-        }}
-      >
+      <button type="submit" onClick={() => handleClickBtn()}>
         Submit
       </button>
       <br />
       <br />
-      Hello To do List name ={name}
+      <div>--------List todo: ---------------</div>
+      {listTodo.map((item, index) => {
+        return (
+          <div
+            id={item.id}
+            key={item.id}
+            onClick={() => handleDeletedTodo(item.id)}
+          >
+            {item.name}
+          </div> //thẻ div này là nơi hiển thị giá trị mới submit
+        );
+      })}
     </div>
   );
 };
