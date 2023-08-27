5171c336a36867f2fe583e483275e234f27e0573
Truong <tranquoctruong812@gmail.com>
2023-08-27 10:40:50 +0700

    scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import Home from "./components/Home"; //import tu ben kia qua
 import AddNewProduct from "./components/AddNewProduct";
 
