1f47d62e443435b363c6972641924ddfcbbaafba
Truong <tranquoctruong812@gmail.com>
2023-09-02 09:55:29 +0700

    get-api

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// const Weather = () => {
-//   return <div>weather okokokoko</div>;
-// };
+const baseURL = "https://dog.ceo/api/breeds/image/random";
 
-class Weather extends React.Component {
-  async componentDidMount() {
-    let response = await axios.get("https://dog.ceo/api/breeds/image/random");
-    console.log("check data", response.data);
-  }
-  render() {
-    return <div> inside weather </div>;
-  }
+const Weather = () => {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setMessage(response.data.message);
+    });
+  }, []);
+  if (!message) return null;
+  return <div>weather okokokoko : message= {message}</div>;
+};
 
-  //   async componentDidMount() {
-  //     let data = await axios({
-  //       method: "post",
-  //       url: "https://reacthook-hoidanit-backend.herokuapp.com/get-data-by-url",
-  //       data: { url: "link api" },
-  //     });
-  //     console.log(data);
-  //   }
-  //   render() {
-  //     return <div> inside weather </div>;
-  //   }
-}
+// class Weather extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     //các biến cần lấy
+//     this.state = {
+//       message: "",
+//     };
+//   }
+
+//   // hàm này chạy sau khi render nên gọi api thì để hàm này là đúng
+//   async componentDidMount() {
+//     let response = await axios.get("https://dog.ceo/api/breeds/image/random");
+//     console.log("check data", response.data);
+
+//     this.setState({
+//       message: response.data.message,
+//     });
+//   }
+//   render() {
+//     return <div> inside weather: {this.state.message} </div>;
+//   }
+
+//   //   async componentDidMount() {
+//   //     let data = await axios({
+//   //       method: "post",
+//   //       url: "https://reacthook-hoidanit-backend.herokuapp.com/get-data-by-url",
+//   //       data: { url: "link api" },
+//   //     });
+//   //     console.log(data);
+//   //   }
+//   //   render() {
+//   //     return <div> inside weather </div>;
+//   //   }
+// }
 
 export default Weather;
